fdbf3e7236cc9b4e20f01adaf1f4366319ae420f
roarcurry <675478140@qq.com>
2019-08-16 22:21:05 +0800

    add blog link

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+	        Just An Aimless Deployment For My VPS.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://roarcurry.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Blog
         </a>
       </header>
     </div>
